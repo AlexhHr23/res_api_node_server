a68406b69bc993e50c1ed86b236b3afca1a271ba
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-05-03 13:02:09 -0600

    Validando crear producto en el servidor

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,7 +1,24 @@
 import {Request, Response} from 'express'
+import { check, validationResult } from 'express-validator';
 import Product from '../models/Product.module';
 
 export const createProduct = async(req: Request, res: Response) => {
+
+    //Validaciob en handler
+    await check('name')
+            .notEmpty().withMessage('El nombre del producto no puede ir vacio')
+            .run(req)
+            
+    await check('price')
+            .isNumeric().withMessage('Valor no valido ')
+            .notEmpty().withMessage('El precio del producto no puede ir vacio')
+            .custom((value) => value > 0).withMessage('Precio no válido')
+            .run(req)
+
+    let errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()})
+    }
 
     const product = await  Product.create(req.body)
 
