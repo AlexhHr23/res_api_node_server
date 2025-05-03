85ab86d4ff6eef92b88fc9167c89a6873677b28e
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-05-03 13:28:26 -0600

    Middleware en express

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,26 +1,13 @@
 import {Request, Response} from 'express'
-import { check, validationResult } from 'express-validator';
 import Product from '../models/Product.module';
 
 export const createProduct = async(req: Request, res: Response) => {
 
-    //Validaciob en handler
-    await check('name')
-            .notEmpty().withMessage('El nombre del producto no puede ir vacio')
-            .run(req)
-            
-    await check('price')
-            .isNumeric().withMessage('Valor no valido ')
-            .notEmpty().withMessage('El precio del producto no puede ir vacio')
-            .custom((value) => value > 0).withMessage('Precio no válido')
-            .run(req)
-
-    let errors = validationResult(req)
-    if(!errors.isEmpty()) {
-        return res.status(400).json({errors: errors.array()})
+    try {
+        const product = await  Product.create(req.body)
+        res.json({data: product})
+    } catch (error) {
+        console.log(error);
     }
 
-    const product = await  Product.create(req.body)
-
-    res.json(({data: product}))
 }
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,8 @@
 
 import {Router} from 'express'
+import { body } from 'express-validator'
 import { createProduct } from './handlers/product'
+import { hadleInputErrors } from './middleware'
 
 
 const router = Router()
@@ -9,7 +11,19 @@ const router = Router()
 //Routing
 // router.get('/', createProduct)
 
-router.post('/', createProduct)
+router.post('/', 
+
+
+    //Validaciob en handler
+        body('name')
+                .notEmpty().withMessage('El nombre del producto no puede ir vacio'),
+                
+        body('price')
+                .isNumeric().withMessage('Valor no valido ')
+                .notEmpty().withMessage('El precio del producto no puede ir vacio')
+                .custom((value) => value > 0).withMessage('Precio no válido'),
+                hadleInputErrors,
+    createProduct)
 
 router.put('/', (req, res) => {
 
