1950d7a985289a8fb4fa7d3be48a66a531c98ec4
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-05-04 15:28:49 -0600

    Documentando productos y atributos de productos

--- src/router.ts
+++ src/router.ts
@@ -7,6 +7,34 @@ import { hadleInputErrors } from './middleware'
 
 const router = Router()
 
+/***
+ * @swagger
+ * components:
+ *      schemas:
+ *          Product:
+ *              type: object
+ *              properties:
+ *                  id: 
+ *                      type: integer
+ *                      description: The product ID
+ *                      example: 1
+ *                  name:
+ *                      type: string
+ *                      description: The product name
+ *                      example: Monitor Curvo de 49 pulgadas
+ *                  price:
+ *                      type: number
+ *                      description: The product price
+ *                      example: 300
+ *                  availability:
+ *                      type: boolean
+ *                      description: The product availability
+ *                      example: true
+ * 
+ * 
+ */
+
+
 
 //Routing
 
